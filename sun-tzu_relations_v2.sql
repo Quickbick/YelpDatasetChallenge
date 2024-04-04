f2d35d416b56c160d0fcf061c1a08782a3e6b117
Quickbick <nsostheller@gmail.com>
2024-04-04 03:33:48 -0700

    fixed typos in relations

--- sun-tzu_relations_v2.sql
+++ sun-tzu_relations_v2.sql
@@ -1,52 +1,51 @@
 -- DDL SQL Statements for Milestone 2, from ER based on Appendix A,
 
--- COMPLETE:
 CREATE TABLE Business (
   id CHAR(22),
-  name VARCHAR(90), -- 90 varchar because names of businesses could be long, I believe.
-  streetAddress	VARCHAR(40),
-  city	VARCHAR(20),
-  state	VARCHAR(2), -- Put down as two for AK, AR state abbreviations.
-  zipcode, CHAR(5)
-  numReviews INT,
-  numCheckins INT,
-  reviewRating FLOAT,
-  businessStars INT,
-  CHECK (reviewRating <= 5), -- If you want to check that businesses must be less than or equal to 5 stars.
+  name VARCHAR(90),
+  street_add VARCHAR(40),
+  city VARCHAR(20),
+  state	 VARCHAR(2),
+  zipcode CHAR(5),
+  num_reviews INT,
+  num_checkins INT,
+  review_rating FLOAT,
+  business_stars INT,
+  CHECK (review_rating <= 5),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE Checkins (
   business CHAR(22),
-  datetime DATETIME,
+  datetime TIMESTAMP,
   count INT,
-  PRIMARY KEY (buisness, datetime),
-  FOREIGN KEY (buisness) REFERENCES Business(id)
+  PRIMARY KEY (business, datetime),
+  FOREIGN KEY (business) REFERENCES Business(id)
 );
 
 CREATE TABLE Attributes (
   business CHAR(22),
-  attributeName VARCHAR(50),
-  attributeValue BOOLEAN,
-  PRIMARY KEY (buisness, attributeName),
-  FOREIGN KEY (buisness) REFERENCES Business(id)
+  attribute_name VARCHAR(50),
+  attribute_value BOOLEAN,
+  PRIMARY KEY (business, attribute_name),
+  FOREIGN KEY (business) REFERENCES Business(id)
 );
 
 CREATE TABLE Categories (
   business CHAR(22),
   category VARCHAR(50),
-  PRIMARY KEY (buisness, category),
-  FOREIGN KEY (buisness) REFERENCES Business(id)
+  PRIMARY KEY (business, category),
+  FOREIGN KEY (business) REFERENCES Business(id)
 );
 
 CREATE TABLE Reviews (
   id CHAR(22),
-  starRating INT,
+  star_rating INT,
   date DATE, 
   text VARCHAR(200),
-  voteType VARCHAR(10),
+  vote_type VARCHAR(10),
   PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE Rating (
   review CHAR(22),
